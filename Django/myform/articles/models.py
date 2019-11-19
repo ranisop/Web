3f9a5817b7edaba1381cad7c75c43dab909722a6
ranisop <dizyobu65@gmail.com>
2019-11-19 22:53:06 +0900

    20191118 Django M:N

--- Django/myform/articles/models.py
+++ Django/myform/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Article(models.Model):
@@ -6,4 +7,21 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+    # 1:N (User:Article)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # Article을 작성한 user -> Article.user
+    # User가 작성한 모든 Article -> user.article_set.all() -> related_name 설정 안했을 때 ** <<코드중복>>
+    # M:N [Like] - User:Article
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    # Article에 좋아요 누른 모든 user -> article.like.users.all()
+    # User가 좋아요 누른 모든 Article -> user.article_set.all() -> related_name 설정 안했을 때 ** <<코드중복>>
+    # 코드가 중복되기 때문에 related_name을 설정함
+    # 설정 후 : user.article_set.all() -> user.like_articles.all()로 변경됨
+
+
+
+
+# Auth
+# 1. Authetication (인증)
+# 2. Authorization (권한)
